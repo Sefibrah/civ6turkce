cfa0fcf704f5030ced31b668d73fd078dca33783
IBRAHIM SEFER <seferibrahim2@gmail.com>
2024-10-26 20:15:20 +0300

    Change from Polish to English

--- Civilization VI turkce/LocalizationDatabase_Data.sql
+++ Civilization VI turkce/LocalizationDatabase_Data.sql
@@ -1,14 +1,14 @@
 -- Installation via user Civilization VI Text folder --
 
 -- Add so that language is at the top of the list --
-UPDATE "Languages" SET Locale = 'tr_TR', Name = 'Türkçe' WHERE Locale = 'pl_PL';
--- But also restore option to choose Polish --
-INSERT OR IGNORE INTO "Languages" (Locale, Name, Collator, PluralRule, SupportsSmallCaps) VALUES('pl_PL', 'Polish', null, 2, 1);
-INSERT OR IGNORE INTO "Languages" (Locale, Name, PluralRule, SupportsSmallCaps) VALUES('pl_PL', 'Polish', 2, 1);
+UPDATE "Languages" SET Locale = 'tr_TR', Name = 'Turkish' WHERE Locale = 'en_US';
+-- But also restore option to choose English --
+INSERT OR IGNORE INTO "Languages" (Locale, Name, Collator, PluralRule, SupportsSmallCaps) VALUES('en_US', 'English', null, 2, 1);
+INSERT OR IGNORE INTO "Languages" (Locale, Name, PluralRule, SupportsSmallCaps) VALUES('en_US', 'English', 2, 1);
 
-INSERT INTO "DefaultAudioLanguages" VALUES('tr_TR', 'Polish(PL)');
+INSERT INTO "DefaultAudioLanguages" VALUES('tr_TR', 'English(US)');
 INSERT INTO "SteamLanguages" VALUES('turkish', 'tr_TR');
 INSERT INTO "LanguagePriorities" VALUES('tr_TR', 'tr_TR', 100);
-INSERT INTO "LanguagePriorities" VALUES('tr_TR', 'pl_PL', 90);
-INSERT INTO "LanguagePriorities" VALUES('pl_PL', 'tr_TR', 90);
+INSERT INTO "LanguagePriorities" VALUES('tr_TR', 'en_US', 90);
+INSERT INTO "LanguagePriorities" VALUES('en_US', 'tr_TR', 90);
 INSERT INTO "FontStyleSheets" VALUES('tr_TR', 'Civ6_FontStyles_EFIGS.xml', null, 'MinionPro-Medium.otf');
